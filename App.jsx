50c0611999dc23397b78f8535f6f8ced01c52ea9
Shreyas Kulat <79224603+shreyaskulat@users.noreply.github.com>
2023-10-02 21:26:05 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Remainder from './BirthdayReminder/Remainder'
+const App = () => {
+  return (
+    <div>
+      <Remainder/>
+    </div>
+  )
+}
+
+export default App
